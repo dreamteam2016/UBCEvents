7f25231143023b870e55400610c9ff59020e17ae
Winnie Wong <winwong03@gmail.com>
2016-02-27 23:11:26 -0800

    defined events model

---
+++ app/models/events.js
@@ -0,0 +1,10 @@
+import DS from 'ember-data';
+
+export default DS.Model.extend({
+  name: DS.attr('string'),
+  location: DS.attr('string'),
+  roomNumber: DS.attr('string'),
+  date: DS.attr('number'),
+  time: DS.attr('number'),
+  category: DS.attr('string')
+});
