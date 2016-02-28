f9f8d7dd0835195aaaae713d8d8160a5d9fdb773
Winnie Wong <winwong03@gmail.com>
2016-02-27 23:56:45 -0800

    redefined event db to have more attributes

--- app/models/event.js
+++ app/models/event.js
@@ -3,8 +3,9 @@ import DS from 'ember-data';
 export default DS.Model.extend({
   name: DS.attr('string'),
   location: DS.attr('string'),
-  roomNumber: DS.attr('number'),
-  date: DS.attr('number'),
-  time: DS.attr('number'),
-  category: DS.attr('string')
+  roomNumber: DS.attr('string'),
+  eventDate: DS.attr('number'),
+  eventTime: DS.attr('number'),
+  category: DS.attr('string'),
+  createdAt: DS.attr('date')
 });
