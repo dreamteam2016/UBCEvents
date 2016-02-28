1b7daa293122463f205cac222268c8244adb7258
sally pang <sallyxpang_@live.com>
2016-02-28 11:29:08 -0800

    deploy

--- app/models/event.js
+++ app/models/event.js
@@ -43,45 +43,6 @@ export default DS.Model.extend({
 		async: true,
 		inverse: 'event',
 		embedded: 'always'
-	}),
-	monthToString: function() {
-		var month = this.get('eventDay').substring(0,1);
-		if (month === '01') {
-			return "January";
-		} 
-		if (month === '02') {
-			return "February";
-		} 
-		if (month === '03') {
-			return "March";
-		}
-		if (month === '04') {
-			return "April";
-		}
-		if (month === '05') {
-			return "May";
-		}
-		if (month === '06') {
-			return "June";
-		}
-		if (month === '07') {
-			return "July";
-		}
-		if (month === '08') {
-			return "August";
-		}
-		if (month === '09') {
-			return "September";
-		}
-		if (month === '10') {
-			return "October";
-		}
-		if (month === '11') {
-			return "November";
-		}
-		if (month === '12') {
-			return "December";
-		}
-	}.property('eventDay')
+	})
 
   });
